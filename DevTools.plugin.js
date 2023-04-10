2cedda3284e315ba4ee633bac6503cb0ede4ad5a
Kurumi <88696837+DungFX@users.noreply.github.com>
2023-04-10 20:03:20 +0700

    Dev tool for better discord
    this is use for better discord

---
+++ DevTools.plugin.js
@@ -0,0 +1,193 @@
+/**
+ * @name DevTools
+ * @author Ahlawat
+ * @authorId 1025214794766221384
+ * @version 1.1.0
+ * @invite SgKSKyh9gY
+ * @description Get an option to open DevTools by right clicking on the home button.
+ * @website https://tharki-god.github.io/
+ * @source https://github.com/Tharki-God/BetterDiscordPlugins
+ * @updateUrl https://tharki-god.github.io/BetterDiscordPlugins/DevTools.plugin.js
+ */
+/*@cc_on
+@if (@_jscript)
+var shell = WScript.CreateObject("WScript.Shell");
+var fs = new ActiveXObject("Scripting.FileSystemObject");
+var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+var pathSelf = WScript.ScriptFullName;
+shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+} else if (!fs.FolderExists(pathPlugins)) {
+shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+} else if (shell.Popup("Should I move myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+fs.MoveFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)));
+shell.Exec("explorer " + pathPlugins);
+shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+}
+WScript.Quit();
+@else@*/
+module.exports = ((_) => {
+  const config = {
+    info: {
+      name: "DevTools",
+      authors: [
+        {
+          name: "Ahlawat",
+          discord_id: "1025214794766221384",
+          github_username: "Tharki-God",
+        },
+      ],
+      version: "1.1.0",
+      description:
+        "Get an option to open DevTools by right clicking on the home button.",
+      github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+      github_raw:
+        "https://tharki-god.github.io/BetterDiscordPlugins/DevTools.plugin.js",
+    },
+    changelog: [
+      {
+        title: "v0.0.1",
+        items: ["Idea in mind"],
+      },
+      {
+        title: "v0.0.5",
+        items: ["Base Model"],
+      },
+      {
+        title: "Initial Release v1.0.0",
+        items: [
+          "This is the initial release of the plugin :)",
+          "Who uses web discord anyways (ノω<。)ノ))☆.。",
+        ],
+      },
+      {
+        title: "v1.0.1",
+        items: ["Corrected text."],
+      }
+    ],
+    main: "DevTools.plugin.js",
+  };
+  const RequiredLibs = [{
+    window: "ZeresPluginLibrary",
+    filename: "0PluginLibrary.plugin.js",
+    external: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+    downloadUrl: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js"
+  },
+  {
+    window: "BunnyLib",
+    filename: "1BunnyLib.plugin.js",
+    external: "https://github.com/Tharki-God/BetterDiscordPlugins",
+    downloadUrl: "https://tharki-god.github.io/BetterDiscordPlugins/1BunnyLib.plugin.js"
+  },
+  ];
+  class handleMissingLibrarys {
+    load() {
+      for (const Lib of RequiredLibs.filter(lib => !window.hasOwnProperty(lib.window)))
+        BdApi.showConfirmationModal(
+          "Library Missing",
+          `The library plugin (${Lib.window}) needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+          {
+            confirmText: "Download Now",
+            cancelText: "Cancel",
+            onConfirm: () => this.downloadLib(Lib),
+          }
+        );
+    }
+    async downloadLib(Lib) {
+      const fs = require("fs");
+      const path = require("path");
+      const { Plugins } = BdApi;
+      const LibFetch = await fetch(
+        Lib.downloadUrl
+      );
+      if (!LibFetch.ok) return this.errorDownloadLib(Lib);
+      const LibContent = await LibFetch.text();
+      try {
+        await fs.writeFile(
+          path.join(Plugins.folder, Lib.filename),
+          LibContent,
+          (err) => {
+            if (err) return this.errorDownloadLib(Lib);
+          }
+        );
+      } catch (err) {
+        return this.errorDownloadLib(Lib);
+      }
+    }
+    errorDownloadZLib(Lib) {
+      const { shell } = require("electron");
+      BdApi.showConfirmationModal(
+        "Error Downloading",
+        [
+          `${Lib.window} download failed. Manually install plugin library from the link below.`,
+        ],
+        {
+          confirmText: "Download",
+          cancelText: "Cancel",
+          onConfirm: () => {
+            shell.openExternal(
+              Lib.external
+            );
+          },
+        }
+      );
+    }
+    start() { }
+    stop() { }
+  }
+  return RequiredLibs.some(m => !window.hasOwnProperty(m.window))
+    ? handleMissingLibrarys
+    : (([Plugin, ZLibrary]) => {
+      const {  
+        Logger,
+        PluginUpdater,
+      } = ZLibrary;     
+        const { HBCM, LibraryModules: { Dispatcher }, LibraryIcons } = BunnyLib.build(config);    
+      return class DevTools extends Plugin {
+        constructor() {
+          super();        
+        }
+        checkForUpdates() {
+          try {
+            PluginUpdater.checkForUpdate(
+              config.info.name,
+              config.info.version,
+              config.info.github_raw
+            );
+          } catch (err) {
+            Logger.err("Plugin Updater could not be reached.", err);
+          }
+        }
+        onStart() {
+          this.checkForUpdates();
+          this.addMenu();
+        }
+        addMenu() {
+          HBCM.insert(config.info.name, this.makeMenuItem());
+        }
+        makeMenuItem() {
+          return {
+            label: "Open DevTools",
+            id: "dev-tools",
+            icon: () => LibraryIcons.Tools("20", "20"),
+            action: () => {
+              Dispatcher.dispatch({
+                type: "DEV_TOOLS_SETTINGS_UPDATE",
+                settings: {
+                  devToolsEnabled: !0,
+                  displayTools: !0,
+                  showDevWidget: !0
+                }
+              })
+            },
+          };
+        }
+        onStop() {
+          HBCM.remove(config.info.name);
+        }
+      };
+    })(ZLibrary.buildPlugin(config));
+})();
+  /*@end@*/
+
